6bbe18cca46b505b348279834201f3f51108cdd3
moba1 <moba2@proton.me>
2023-10-28 00:51:28 +0900

    add path finding logic

--- function.fish
+++ function.fish
@@ -1,4 +1,16 @@
 function sword --description 'pnpm, yarn, npm wrapper'
   set -l directory (pwd)
-  echo $directory
+
+  while true
+    set -l \
+      package_json_path \
+      (string join / (string trim -c / -r "$directory") package.json)
+    echo $package_json_path
+
+    if [ "$directory" = / ]
+      echo not in Node.js project >&2
+      return 1
+    end
+    set directory (dirname "$directory")
+  end
 end
